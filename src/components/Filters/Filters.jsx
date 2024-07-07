4de3a4760bd8f791e4c3f53ad51429c02014d1b1
Suyeong <81769862+su794@users.noreply.github.com>
2024-07-07 09:53:53 -0700

    Filters

---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,29 @@
+import React, { useContext, useState } from 'react';
+import styles from './Filters.module.css';
+import { FaMoon } from 'react-icons/fa';
+import { IoSunny } from "react-icons/io5";
+import { DarkModeContext, useDarkMode } from '../../context/DarkModeContext';
+
+export default function Filters( { filters, onFilter } ) {
+    const [currentFilter, setCurrentFilter] = useState(filters[0]);
+    const handleFilter = (e) => {
+        e.preventDefault();
+        onFilter('active');
+    }
+
+    const { darkMode, toggleDarkMode } = useDarkMode();
+  return (
+    <header>
+      <div>
+        <button onClick={() => toggleDarkMode()}>
+          { darkMode ? <IoSunny /> : <FaMoon /> }
+        </button>
+      </div>
+      <ul className={styles.filters}>
+        {
+            filters.map( filter => <li className={styles.filter} key={filter}><button onClick={ () => { onFilter(filter) }}>{filter}</button></li> )
+        }
+      </ul>
+    </header>
+  )
+}
