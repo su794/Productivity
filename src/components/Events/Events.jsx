450e0d8c1aeee24dd63b861b75aec76dd3d9e63d
Suyeong <81769862+su794@users.noreply.github.com>
2024-09-08 15:46:35 -0700

    Changed the way to add an event to the calendar, from adding title, start and end dates to cliking the date on the calendar.

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -7,6 +7,8 @@ import getDay from "date-fns/getDay";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { v4 as uuidv4 } from 'uuid';
+import EventModal from '../EventModal/EventModal';
 
 const locales = {
     "en-US": require("date-fns/locale/en-US")
@@ -24,43 +26,75 @@ export default function Events() {
 
   const [events, setEvents] = useState(() => readEventsFromLocalStorage());
 
+  const [text, setText] = useState('');
   const [startDate, setStartDate] = useState( null );
+  const [startDatePicker, setStartDatePicker] = useState('');
+  const [endDatePicker, setEndDatePicker] = useState('');
 
-  console.log(startDate);
+  const [modalStatus, setModalStatus] = useState('modal-close');
 
   useEffect( () => {
     localStorage.setItem('events', JSON.stringify(events));
-    
   }, [events] )
 
-  const [newEvent, setNewEvent] = useState({title: "", start: "", end: ""});
+  const [newEvent, setNewEvent] = useState({id: uuidv4(), title: "", start: "", end: ""});
 
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
   function handleAddEvent() {
+    if(text.trim().length === 0) {
+      
+      return;
+    }
     setEvents([...events, newEvent]);
 
     localStorage.setItem('events', JSON.stringify(newEvent));
+    setText('');
+    setStartDatePicker('');
+    setEndDatePicker('');
   }
 
+  const handleSelect = ({start, end}) => {
+    setModalStatus('modal-open');
+    //const title = window.prompt("New Event Name");
+    
+    if( text.trim().length === 0 ) {
+      alert('Please add the event title');
+      return;
+    
+    }
+
+    // setNewEvent({title, start: start.toString(), end: end.toString()});
+    setEvents([...events, {title: text, start, end}]);
+
+    localStorage.setItem('events', JSON.stringify(newEvent));
+
+    setText('');
+    
+  }
   return (
     <>
         <h2>Add new Event</h2>
         <div>
-            <input 
-            type="text" 
-            placeholder='Add Title' 
-            style={{ width: "20%", marginRight: "10px" }}
-            value={newEvent.title}
-            onChange={ (e) => setNewEvent({...newEvent, title: e.target.value})}
+            <input
+              type="text" 
+              placeholder='Add Title' 
+              style={{ width: "20%", marginRight: "10px" }}
+              value={text}
+              onChange={handleTextChange}
             />
             <DatePicker 
             placeholderText="Start Date"
             style={{ marginRight: "10px" }}
             selected={newEvent.start.toString()}
             onChange={(start) => { 
-                setNewEvent({...newEvent, start}) 
+                setNewEvent({...newEvent, title: text, start}) 
                 setStartDate(start);
+                setStartDatePicker(start.toLocaleDateString());
               } 
             }
+            value={startDatePicker}
             />
             <DatePicker 
             placeholderText="End Date"
@@ -68,33 +102,31 @@ export default function Events() {
             selected={newEvent.end.toString()}
             onChange={(end) => {
                 end.setUTCHours(23,59,59,999);
-                setNewEvent({...newEvent, end}) 
+                setNewEvent({...newEvent, title: text, end});
+                setEndDatePicker(end.toLocaleDateString() );
               }
             }
+            value={endDatePicker}
             />
-            <button
+            {/* <button
               onClick={handleAddEvent}
             >
             Add event
-            </button>
+            </button> */}
         </div>
-        <Calendar 
-            localizer={localizer} 
+        <Calendar
+            views={["month"]}
+            selectable
+            localizer={localizer}
+            defaultDate={new Date()}
             events={events} 
             startAccessor="start"
             endAccessor="end"
             style={{ height: 700, margin: "50px" }}
+            onSelectSlot={handleSelect}
         />
-
-        <ul>
-        {
-          
-          events.map( (eventTodo, i) => <li key={i}>{ eventTodo.start ? eventTodo.start.toString() : 'no-start'} { eventTodo.end ? eventTodo.end.toString() : 'no-end'}</li> )
-          
-        }
-        </ul>
+        <EventModal modalStatus={modalStatus} />
         
-
     </>
     
   )
@@ -106,22 +138,13 @@ function readEventsFromLocalStorage() {
     if (k === "start" || k === "end" ) {
       
       return new Date(v);
-    } 
-    // else if( k === "end" ) {
-    //     // get indexof 'T' to replace time(hours);
-    //     const getIndexOfT = v.indexOf('T');
-
-    //     // Replace '07' to '23'
-    //     let newEventEndDate = `${v.substring(0,getIndexOfT+1)}23${v.substring(getIndexOfT+3)}`;
-        
-    //     return new Date(newEventEndDate);
-      
-    // }
+    }
     return v;
   }
     const storedEvents = localStorage.getItem('events');
 
     let parsedEvents = JSON.parse(storedEvents, parseDate);
 
+    console.log(parsedEvents);
     return storedEvents ? parsedEvents : [];
 }
